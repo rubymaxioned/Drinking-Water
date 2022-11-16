fd287beac3fe44c921896163fe3237b508fa7544
rubymaxioned <axioned0482022>
2022-11-16 19:39:58 +0530

    added color for small glasses

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,15 @@ var remaining = document.querySelector(".remaining");
 var percentage = document.querySelector(".percentage");
 var smallGlass = document.querySelectorAll(".small-glasses");
 console.log(smallGlass);
+
+smallGlass.forEach(function(list,index){
+    
+    list.addEventListener('click',function(){
+        console.log(list);
+    console.log(index);
+        for(var i=0; i<=index; i++){
+            console.log(smallGlass[i]);
+            smallGlass[i].classList.add('color');
+        }
+    })
+})
