129572a08c35932bdb012875b1b0aa536a7b18d8
rubymaxioned <axioned0482022>
2022-11-17 10:55:22 +0530

    big glass height

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,23 @@ smallGlass.forEach(function (list, index) {
     list.addEventListener('click', function () {
         console.log(list);
         console.log(index);
+        var percentageContent = 0;
+        var glassHeight = 0;
         for (var i = 0; i <= index; i++) {
             console.log(smallGlass[i]);
             smallGlass[i].classList.add('color');
+            percentageContent += 12.5;
+            console.log(percentageContent);
+            percentage.innerText = percentageContent+"%";
+            glassHeight += 25.5;
+            console.log(glassHeight)
         }
         for (var i = index + 1; i <= 7; i++) {
             console.log(smallGlass[i]);
             smallGlass[i].classList.remove('color');
         }
+        percentage.style.visibility = "visible";
+        percentage.style.height = `${glassHeight}px`;
+        console.log(percentage.style.height);
     })
 })
