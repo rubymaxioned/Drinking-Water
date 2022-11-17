5808954f3d54275ad43ef100cd1fa0a7d320808b
rubymaxioned <axioned0482022>
2022-11-17 11:37:01 +0530

    big glass liter

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,9 @@ smallGlass.forEach(function (list, index) {
         console.log(index);
         var percentageContent = 0;
         var glassHeight = 0;
+        var literHeight = 0;
+        var remainingHeight = 0;
+        var literText = 0;
         for (var i = 0; i <= index; i++) {
             console.log(smallGlass[i]);
             smallGlass[i].classList.add('color');
@@ -19,14 +22,23 @@ smallGlass.forEach(function (list, index) {
             console.log(percentageContent);
             percentage.innerText = percentageContent+"%";
             glassHeight += 25.5;
-            console.log(glassHeight)
+            literHeight += 10;
+            remainingHeight += 12.5;
+            literText += 0.25;
+            var literHeightFinal = 75 - literHeight; 
+            var remainingHeightFinal = 100 - remainingHeight;
+            var literTextFinal = literText;
+            console.log(literTextFinal);
         }
         for (var i = index + 1; i <= 7; i++) {
             console.log(smallGlass[i]);
             smallGlass[i].classList.remove('color');
         }
+        liter.innerText = literTextFinal + "L";
         percentage.style.visibility = "visible";
         percentage.style.height = `${glassHeight}px`;
+        // liter.style.top = `${literHeightFinal}px`;
+        // remaining.style.top = `${remainingHeightFinal}px`;
         console.log(percentage.style.height);
     })
 })
