c17fa7da955360078c55d67b5e882b92e268a80a
rubymaxioned <axioned0482022>
2022-11-17 12:14:14 +0530

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,45 +4,34 @@ var remaining = document.querySelector(".remaining");
 var percentage = document.querySelector(".percentage");
 var smallGlass = document.querySelectorAll(".small-glasses");
 var reminder = document.querySelector(".reminder");
-// console.log(smallGlass);
+
 
 smallGlass.forEach(function (list, index) {
 
     list.addEventListener('click', function () {
-        // console.log(list);
-        // console.log(index);
         var percentageContent = 0;
         var glassHeight = 0;
-        // var literHeight = 0;
-        // var remainingHeight = 0;
         var reminderHeight = 0;
-        var literText = 0;
+        var literText = 2;
+
         for (var i = 0; i <= index; i++) {
-            console.log(smallGlass[i]);
             smallGlass[i].classList.add('color');
             percentageContent += 12.5;
-            // console.log(percentageContent);
             percentage.innerText = percentageContent+"%";
             glassHeight += 25.5;
             reminderHeight += 10;
-            // literHeight += 10;
-            // remainingHeight += 12.5;
-            literText += 0.25;
+            literText -= 0.25;
             var reminderHeightFinal = 75 - reminderHeight;
-            // var literHeightFinal = 75 - literHeight; 
-            // var remainingHeightFinal = 100 - remainingHeight;
             var literTextFinal = literText;
-            // console.log(literTextFinal);
         }
+
         for (var i = index + 1; i <= 7; i++) {
-            console.log(smallGlass[i]);
             smallGlass[i].classList.remove('color');
         }
+        
         liter.innerText = literTextFinal + "L";
         percentage.style.visibility = "visible";
         percentage.style.height = `${glassHeight}px`;
-        // liter.style.top = `${literHeightFinal}px`;
-        // remaining.style.top = `${remainingHeightFinal}px`;
         reminder.style.top = `${reminderHeightFinal}px`;
         console.log(percentage.style.top);
     })
