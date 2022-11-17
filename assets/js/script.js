b3b502720a44089184700fb0f38d61f3501986d3
rubymaxioned <axioned0482022>
2022-11-17 12:07:01 +0530

    reminder

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,36 @@ var liter = document.querySelector(".liter");
 var remaining = document.querySelector(".remaining");
 var percentage = document.querySelector(".percentage");
 var smallGlass = document.querySelectorAll(".small-glasses");
-console.log(smallGlass);
+var reminder = document.querySelector(".reminder");
+// console.log(smallGlass);
 
 smallGlass.forEach(function (list, index) {
 
     list.addEventListener('click', function () {
-        console.log(list);
-        console.log(index);
+        // console.log(list);
+        // console.log(index);
         var percentageContent = 0;
         var glassHeight = 0;
-        var literHeight = 0;
-        var remainingHeight = 0;
+        // var literHeight = 0;
+        // var remainingHeight = 0;
+        var reminderHeight = 0;
         var literText = 0;
         for (var i = 0; i <= index; i++) {
             console.log(smallGlass[i]);
             smallGlass[i].classList.add('color');
             percentageContent += 12.5;
-            console.log(percentageContent);
+            // console.log(percentageContent);
             percentage.innerText = percentageContent+"%";
             glassHeight += 25.5;
-            literHeight += 10;
-            remainingHeight += 12.5;
+            reminderHeight += 10;
+            // literHeight += 10;
+            // remainingHeight += 12.5;
             literText += 0.25;
-            var literHeightFinal = 75 - literHeight; 
-            var remainingHeightFinal = 100 - remainingHeight;
+            var reminderHeightFinal = 75 - reminderHeight;
+            // var literHeightFinal = 75 - literHeight; 
+            // var remainingHeightFinal = 100 - remainingHeight;
             var literTextFinal = literText;
-            console.log(literTextFinal);
+            // console.log(literTextFinal);
         }
         for (var i = index + 1; i <= 7; i++) {
             console.log(smallGlass[i]);
@@ -39,6 +43,7 @@ smallGlass.forEach(function (list, index) {
         percentage.style.height = `${glassHeight}px`;
         // liter.style.top = `${literHeightFinal}px`;
         // remaining.style.top = `${remainingHeightFinal}px`;
-        console.log(percentage.style.height);
+        reminder.style.top = `${reminderHeightFinal}px`;
+        console.log(percentage.style.top);
     })
 })
